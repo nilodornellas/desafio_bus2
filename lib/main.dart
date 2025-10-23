9c9048a30aec8dafcc7fd04b31b57c0b08b8f1de
Nilo Dornellas <iannilo01@gmail.com>
2025-10-23 15:23:27 -0300

    feat: update app title to 'Desafio Bus2'

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Desafio Bus2',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
       ),
